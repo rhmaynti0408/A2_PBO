aa14aa0a1fc554a06771c71e616ccca4c51fab6d
2009106068_Rahmayanti <rhmaynti0408@gmail.com>
2022-04-12 19:57:02 +0800

    posttest3

---
+++ 2009106068_Rahmayanti/src/posttest3/Cons_Produk.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package posttest3;
+
+
+
+/**
+ *
+ * @author ASUS
+ */
+public class Cons_Produk {
+
+    public Cons_Produk(String nama, int harga, String ukuran, int id) {
+        this.nama = nama;
+        this.harga = harga;
+        this.ukuran = ukuran;
+        this.id = id;
+    }
+    String nama;
+    int harga;
+    String ukuran;
+    int id;
+}
---
+++ 2009106068_Rahmayanti/src/posttest3/method.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package posttest3;
+
+/**
+ *
+ * @author ASUS
+ */
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class method {
+    //membuat fungsi untuk 
+    public static int GetInt(){
+        Scanner input = new Scanner (System.in);
+        while (true){
+            try{
+                return input.nextInt();
+            }
+            catch (InputMismatchException e){
+                input.nextLine();
+                System.out.println("Input Salah Coba Lagi: ");
+            }
+        }
+    }
+    //membuat fungsi untuk menampilkan list data.
+    static void list_data(ArrayList<Produk> ListProduk){
+        System.out.println("===List Produk iBox===");
+        System.out.println("No ID Nama\t\tUkuran\t\t\tHarga");
+        for(int i=0;i<ListProduk.size();i++){
+            System.out.println((i+1)+" "+ListProduk.get(i).getId()+" "+ListProduk.get(i).getNama()+"\t\t"+ListProduk.get(i).getUkuran()+"\t\t"+ListProduk.get(i).getHarga());
+        }
+        System.out.println("======================");
+    }
+}
