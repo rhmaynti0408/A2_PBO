e322ed3baf8a7a44b263f894927b84b9faf488b4
2009106068_Rahmayanti <rhmaynti0408@gmail.com>
2022-04-30 05:05:04 +0800

    Posttest 4

---
+++ 2009106068_Rahmayanti/src/posttest4/Main.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package posttest4;
+import posttest3.*;
+import posttest2.*;
+import java.util.*;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner input = new Scanner (System. in);
+        ArrayList<Produk> ListProduk = new ArrayList<>();
+        int id=0;
+        while (true){
+            System.out.println("====== DATA PRODUK IBOX ======");
+            System.out.println("(1). Daftar Produk Baru");
+            System.out.println("(2). Lihat List Produk");
+            System.out.println("(3). Ubah Data Produk");
+            System.out.println("(4). Hapus Data Produk");
+            System.out.println("(5). Keluar");
+            System.out.println("============================");
+            System.out.println("Pilih Option (Angka) : ");int a=method.GetInt();
+            if (a!=5){
+                if(a==1){
+                    int i;
+                    
+                    System.out.println("==== LIST PRODUK IBOX ====");
+                    
+                    System.out.print("Nama\t: ");String Nama=(input.nextLine());
+                    System.out.print("Harga\t: ");int Harga =(method.GetInt());
+                    System.out.print("Ukuran\t: ");String Ukuran = (input.nextLine());
+                    ListProduk.add(new Produk(Nama,Harga,Ukuran,id+1));
+                    id++;
+                }
+                else if(a==2){
+                    method.list_data(ListProduk);
+                }
+                else if(a==3){
+                    method.list_data(ListProduk);
+                    
+                    System.out.println("!!Diharapkan Pencarian Data Produk Sesuai ID Produk!!");
+                    System.out.print(">>");int cari = method.GetInt();
+                    for (int i=0;i<ListProduk.size();i++){
+                        if(cari==ListProduk.get(i).getId()){
+                            System.out.println("CEK ID = "+ListProduk.get(i).getId());
+                            System.out.println("===== Data Sebelumnya =====");
+                            System.out.println("ID\t\t: "+ListProduk.get(i).getId());
+                            System.out.println("Nama\t: "+ListProduk.get(i).getNama());
+                            System.out.println("Ukuran\t: "+ListProduk.get(i).getUkuran());
+                            System.out.println("Harga\t: "+ListProduk.get(i).getHarga());
+                            ListProduk.remove(i);
+//                            id--;
+                            //Mengisi data baru menggunakan constructor
+                            System.out.println("===== Data Baru =====");
+                            System.out.println("ID\t\t: "+id);
+                            System.out.print("Nama\t: ");String nama = input.nextLine();
+                            System.out.print("Harga\t: ");int harga = method.GetInt();
+                            System.out.print("Ukuran\t: ");String ukuran = input.nextLine();
+                            
+                            Cons_Produk data_baru = new Cons_Produk(nama,harga,ukuran,id);
+                            
+                                                      
+                            ListProduk.add(new Produk(nama,harga,ukuran,id));
+                        }
+                    }
+                    System.out.println("==== LIST PRODUK IBOX ====");
+                    System.out.println("No ID Nama\t\tUkuran\t\t\tHarga");
+                    for(int i=0;i<ListProduk.size();i++){
+                        System.out.println((i+1)+" "+ListProduk.get(i).getId()+" "+ListProduk.get(i).getNama()+"\t\t"+ListProduk.get(i).getUkuran()+"\t\t"+ListProduk.get(i).getHarga());
+                    }
+                    System.out.println("======================");
+                }
+                else if (a==4){
+                    method.list_data(ListProduk);
+                    
+                    System.out.println("!!Pencarian Data Produk Sesuai ID Produk!!");
+                    System.out.print(">>");int cari = method.GetInt();
+                    for (int i=0;i<ListProduk.size();i++){
+                        if(cari==ListProduk.get(i).getId()){
+                            ListProduk.remove(i);
+                            id--;
+                        }
+                    }
+                    method.list_data(ListProduk);
+                }
+                else if (a==5){
+                    System.out.println("=== System Shut ===");
+                    System.exit(1);
+                }
+            }
+        }
+    }
+}
---
+++ 2009106068_Rahmayanti/src/posttest4/Produk.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package posttest4;
+
+import posttest3.*;
+
+
+//posttest 3 enkapsulasi dan setter getter
+/**
+ *
+ * @author ASUS
+ */
+public class Produk {
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    public void setUkuran(String ukuran) {
+        this.ukuran = ukuran;
+    }
+
+    public Produk(String nama, int harga, String ukuran, int id) {
+        this.nama = nama;
+        this.harga = harga;
+        this.ukuran = ukuran;
+        this.id = id;
+    }
+
+    protected String nama; //enkapsulasi
+    protected int harga;
+    protected String ukuran;
+    protected int id;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public String getUkuran() {
+        return ukuran;
+    }
+}
+
+
